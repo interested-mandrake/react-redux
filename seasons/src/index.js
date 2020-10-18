f6f4116d5492dc38a78fe1483c43e5f6f9be6261
interested-mandrake <ssepanski343@gmail.com>
2020-10-18 14:32:20 -0500

    add display error message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// the issue with the functional component here was that it takes
-// some time to get the value back after calling the geolocation
-// service. the jsx will get loaded before the getCurrentPosition method
-// returns a value. with a functional component, we had no good way
-// of re-rendering or telling the App component to pause its rendering
-// process
 class App extends React.Component{
    constructor(props) { // in react, constructor is called with props object
         super(props); // construct the parent
 
-        this.state = { latitude: null }; // set state equal to an object
+        this.state = { latitude: null, errorMessage: ''}; // set state equal to an object
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ latitude: position.coords.latitude });
             }, //success callback
-            (err) => console.log(err) //error callback
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            } //error callback
         ); // callbacks run in the future, in this case
-           // after exiting the constructor
+           // after exiting the constructor method
     } 
    
     render(){     
-        return <div>Latitude: {this.state.latitude}</div>;
+        return (
+            <div>
+                Latitude: {this.state.latitude}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
-} // in this app as it is, render method gets called twice, once when it
-// gets constructed, and once when state is null, and again
-// when this.setState gets called
+} 
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
