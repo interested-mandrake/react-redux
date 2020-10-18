e04c0afda637cb6d2a6e3d0a943a14a53fd2aceb
interested-mandrake <ssepanski343@gmail.com>
2020-10-18 14:20:31 -0500

    add notes on callbacks and state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -14,15 +14,20 @@ class App extends React.Component{
         this.state = { latitude: null }; // set state equal to an object
 
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), //success fallback
-            (err) => console.log(err) //error falback
-        );
-   } 
+            (position) => {
+                this.setState({ latitude: position.coords.latitude });
+            }, //success callback
+            (err) => console.log(err) //error callback
+        ); // callbacks run in the future, in this case
+           // after exiting the constructor
+    } 
    
     render(){     
         return <div>Latitude: {this.state.latitude}</div>;
     }
-}
+} // in this app as it is, render method gets called twice, once when it
+// gets constructed, and once when state is null, and again
+// when this.setState gets called
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
