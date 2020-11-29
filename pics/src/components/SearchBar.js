024764ee98a898faf7424f48f21900ae14ff1fa7
interested-mandrake <ssepanski343@gmail.com>
2020-11-29 11:10:50 -0600

    add event handler as callback method in prop

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange() {
+
+    }
+
     render() {
         return  (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" onChange={this.onInputChange} /> {/* giving the function we defined as a callback function ( if we included parentheses, it would run on render, and we do not want that) */}
                     </div>
                 </form>
             </div> 
